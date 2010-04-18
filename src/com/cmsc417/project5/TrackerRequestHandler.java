f747df82cb2c48cbda90074b1aa3c261e93093c5
Chris <skankinsasquatch@gmail.com>
2010-04-18 19:39:46 -0400

    Fixed a bug with null events, added more javadocs.

--- src/com/cmsc417/project5/TrackerRequestHandler.java
+++ src/com/cmsc417/project5/TrackerRequestHandler.java
@@ -14,6 +14,10 @@ import org.ardverk.coding.BencodingInputStream;
 
 import sun.net.www.content.text.PlainTextInputStream;
 
+/**
+ * @author Chris Kaminski & David Thomas
+ *
+ */
 public class TrackerRequestHandler {
 
 	private String baseURL;
@@ -42,23 +46,31 @@ public class TrackerRequestHandler {
 		this.port = port;
 	}
 
+	
+	/**
+	 * Generate a request to a tracker. Returns a <String, Value> map corresponding
+	 * to the tracker's response. Call with event as null or equal to "" to get
+	 * periodic updates on available peers. uploaded and downloaded are ignored when
+	 * event is equal to "started".
+	 * 
+	 * @param event
+	 * @param uploaded
+	 * @param downloaded
+	 * @param left
+	 * @return
+	 */
 	public Map<String, ?> request(String event, long uploaded, long downloaded,
 									long left) {
 
 		String requestURL = getRequestURL(event, uploaded, downloaded, left);
-
-		URL request = null;
-		URLConnection connection = null;
-		PlainTextInputStream content = null;
-		Map<String, ?> trackerResponse = null;
 		
 		try {
-			request = new URL(requestURL);
-			connection = request.openConnection();
-			content = (PlainTextInputStream) connection.getContent();
+			URL request = new URL(requestURL);
+			URLConnection connection = request.openConnection();
+			PlainTextInputStream content = (PlainTextInputStream) connection.getContent();
 			BencodingInputStream trackerStream = new BencodingInputStream(
 					content);
-			trackerResponse = trackerStream.readMap();
+			Map<String, ?> trackerResponse = trackerStream.readMap();
 			return trackerResponse;
 			
 		} catch (MalformedURLException e) {
@@ -70,12 +82,21 @@ public class TrackerRequestHandler {
 		return null;
 	}
 
+	/**
+	 * Get a URL encoded version of the info hash, since the java implementation 
+	 * won't work for this.
+	 * 
+	 * @param hash
+	 * @return
+	 */
 	private String urlEncodeHash(byte[] hash) {
 
 		String urlEncodedHash = "";
 
+		//Convert all bytes to their hex value and prefix with %
 		for (int i = 0; i < hash.length; i++) {
 			String hexVal = Integer.toHexString(hash[i] & 0xFF);
+			//Pad if the leading zero was cut off
 			if (hexVal.length() != 2) {
 				urlEncodedHash += ("%0" + hexVal);
 			} else {
@@ -86,6 +107,15 @@ public class TrackerRequestHandler {
 		return urlEncodedHash;
 	}
 
+	/**
+	 * Get a formatted request URL for this set of parameters.
+	 * 
+	 * @param event
+	 * @param uploaded
+	 * @param downloaded
+	 * @param left
+	 * @return
+	 */
 	private String getRequestURL(String event, long uploaded, long downloaded,
 									long left) {
 
@@ -94,17 +124,20 @@ public class TrackerRequestHandler {
 		requestURL += "peer_id=" + peerID + "&";
 		requestURL += "port=" + Integer.toString(port) + "&";
 
-		if (!event.equals("started")) {
+		//Make uploaded and downloaded = 0 if the started event is being requested
+		if (event == null || !event.equals("started")) {
 			requestURL += "uploaded=" + Long.toString(uploaded) + "&";
 			requestURL += "downloaded=" + Long.toString(uploaded) + "&";
-		} else {
+		} else if(event != null && event.equals("started")) {
 			requestURL += "uploaded=" + "0&";
 			requestURL += "downloaded=" + "0&";
 		}
 
 		requestURL += "left=" + left + "&";
 		requestURL += "compact=1&";
-		requestURL += "event=" + event;
+		
+		if(event != null && !event.equals(""))
+			requestURL += "event=" + event;
 
 		return requestURL;
 	}
