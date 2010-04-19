43f8d485f65fa704c9e00b6d8595cc63d2c805f8
Chris <skankinsasquatch@gmail.com>
2010-04-19 14:31:03 -0400

    Added driver class for testing and later on for implementing the main torrent class. Added the beginnings of a thread pool for controlling peer interactions.

---
+++ src/com/cmsc417/project5/PeerConnection.java
@@ -0,0 +1,19 @@
+package com.cmsc417.project5;
+
+import java.net.Socket;
+
+public class PeerConnection implements Runnable {
+
+	private Socket socket;
+	
+	public PeerConnection(Socket socket) {
+		this.socket = socket;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
